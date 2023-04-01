f80f8abb0038b913b6e1df31fcfa6162c185e68a
Bazs <scheder.balazs@gmail.com>
2023-04-01 15:09:14 +0200

    [Merger] Concat tables wo duplication

--- merger.py
+++ merger.py
@@ -1,4 +1,7 @@
+import os
+import glob
 from tkinter import *
+import pandas as pd
 from tkinter import messagebox,filedialog,Entry
 
 # ---------------------------- CONSTANTS ------------------------------- #
@@ -8,10 +11,12 @@ FONT_NAME = "Courier"
 MAX_LENGTH = 0 #TODO depricated , kill it
 FILE_PATH = None #TODO depricated , kill it
 OUTPUT_PATH = None 
+OUTPUT_NAME = None
 CSCS_PATH = None
 DOMAIN_COLUMN = None
 DR_COLUMN = None 
 CONTACTES_SITES = None
+
 
 
 # ---------------------------- DIVIDE MECHANISM ------------------------------- # 
@@ -23,19 +28,40 @@ def write_to_file(filename,rows):
 def merge():
     global MAX_LENGTH
     
-    if CSCS_PATH is None:
-        messagebox.showerror(title='Error', message='Please add the path of csv\'s folder!')
-    elif DOMAIN_COLUMN is None:
-        messagebox.showerror(title='Error', message='Please add the name of the domain column!')
-    elif DR_COLUMN is None:
-        messagebox.showerror(title='Error', message='Please add the name of the DR column!')
-    elif CONTACTES_SITES is None:
-        messagebox.showerror(title='Error', message='Please add the path of the contactes site\'s table!')
-    elif OUTPUT_PATH is None:
-        messagebox.showerror(title='Error', message='Please add a the output path of merged table!')
+    #Handle errors
+    # if CSCS_PATH is None: #TODO decomment all of error checks
+    #     messagebox.showerror(title='Error', message='Please add the path of csv\'s folder!')
+    # elif DOMAIN_COLUMN is None:
+    #     messagebox.showerror(title='Error', message='Please add the name of the domain column!')
+    # elif DR_COLUMN is None:
+    #     messagebox.showerror(title='Error', message='Please add the name of the DR column!')
+    # elif CONTACTES_SITES is None:
+    #     messagebox.showerror(title='Error', message='Please add the path of the contactes site\'s table!')
+    # elif OUTPUT_NAME is None:
+    #     messagebox.showerror(title='Error', message='Please add a the name of the output file of merged table!')
+    # elif OUTPUT_PATH is None:
+    #     messagebox.showerror(title='Error', message='Please add a the output path of merged table!')
 
-   
+    #Create the merged csv file and remove the duplications
+    path = CSCS_PATH
+    all_files = os.listdir(CSCS_PATH)
 
+    li = []
+
+    for filename in all_files:
+        if not filename.endswith(".txt"):
+            df = pd.read_csv(CSCS_PATH + '/' + filename, index_col=None, header=0)
+            li.append(df)
+
+    merged_frame = pd.concat(li, axis=0, ignore_index=True).drop_duplicates()
+  
+    #Remove the unneeded columns
+    for column_name in merged_frame.columns.values.tolist():
+        if DOMAIN_COLUMN.strip().lower() != column_name.lower() \
+           and DR_COLUMN.strip().lower() != column_name.lower():
+            del merged_frame[column_name]
+
+    merged_frame.to_csv(OUTPUT_PATH + '/' + OUTPUT_NAME +'.csv',index=False)
 
 
 def select_dir():
@@ -71,7 +97,7 @@ def select_domain_header():
 
 def select_dr_header():
     global DR_COLUMN
-    value = domain_header.get()
+    value = dr_header.get()
     try:
         DR_COLUMN = str(value)
 
@@ -80,6 +106,19 @@ def select_dr_header():
 
     except ValueError:
         messagebox.showerror(title='Error', message='Please add the name of the DR header!')
+
+def set_output_filename():
+    global OUTPUT_NAME
+    value = output_name.get()
+    try:
+        OUTPUT_NAME = str(value)
+
+        if OUTPUT_NAME == '':
+            messagebox.showerror(title='Error', message='Please add the name of the domain header!')    
+
+    except ValueError:
+        messagebox.showerror(title='Error', message='Please add the name of the domain header!')
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -127,16 +166,25 @@ contacted_sites.grid(column=1, row=4)
 csites_save = Button(text="Browse", highlightthickness=0,command=set_contacted_site)
 csites_save.grid(column=2, row=4)
 
-output_label = Label(text="Output:",bg=GREEN, font=(FONT_NAME, 18))
-output_label.grid(column=0, row=5)
+output_name_label = Label(text="Output Filename:",bg=GREEN, font=(FONT_NAME, 18))
+output_name_label.grid(column=0, row=5)
+
+output_name = Entry(window,width=50)
+output_name.grid(column=1, row=5)
+
+output_name_submit = Button(text="Submit", highlightthickness=0,command=set_output_filename)
+output_name_submit.grid(column=2, row=5)
+
+output_label = Label(text="Output Path:",bg=GREEN, font=(FONT_NAME, 18))
+output_label.grid(column=0, row=6)
 
 output_path = Entry(window,width=50)
-output_path.grid(column=1, row=5)
+output_path.grid(column=1, row=6)
 
 output_browse = Button(text="Browse", highlightthickness=0,command=set_output_path)
-output_browse.grid(column=2, row=5)
+output_browse.grid(column=2, row=6)
 
 divide_button = Button(text="Get my list!", highlightthickness=0, command=merge)
-divide_button.grid(column=1, row=6)
+divide_button.grid(column=1, row=7)
 
 window.mainloop()
